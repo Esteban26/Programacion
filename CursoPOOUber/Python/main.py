c8ead67651f472b372b8db1b5ac478dab664859c
Esteban <angelesteba0326@gmail.com>
2022-06-10 11:50:51 -0500

    sigue la continuacion del curso POO

--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,14 +1,30 @@
 from car import Car
+from account import Account
+
 if __name__ == "__main__":
     print('hola mundo')
-    car = Car()
-    car.license = "SDF256"
-    car.driver = "Andres Herrera"
-    car.passengenger = 4
+    car = Car("AFG358",Account("Andres Herrera","AS12356"))
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car.license = "NJR247"
-    car.driver = "Andrea Herrera"
-    car.passengenger = 3
-    print(vars(car))
+
+
+
+
+
+
+
+
+
+
+    # car = Car()
+    # car.license = "SDF256"
+    # car.driver = "Andres Herrera"
+    # car.passengenger = 4
+    # print(vars(car))
+
+    # car2 = Car()
+    # car.license = "NJR247"
+    # car.driver = "Andrea Herrera"
+    # car.passengenger = 3
+    # print(vars(car))
