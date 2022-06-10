7a0f4c1f96ceb4957d9980413cf020e8fa7f77a5
Esteban <angelesteba0326@gmail.com>
2022-06-10 16:58:11 -0500

    sigue el curso de POO

--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
 var Car = new Car("SDF357", new Account("Andres Herrera","AS1336"))
 Car.passenger = 4;
 Car.printDataCar();
+
+var UberX = new UberX("XFG149",new Account("Miguel Hernandez","cc1234"),"Ford","FordFiesta");
+UberX.passenger = 4;
+UberX.printDataCar();
